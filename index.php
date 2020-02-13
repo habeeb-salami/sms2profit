038bec706f0080bb1930ea7ea8127c71eb23bd33
Habeeb Salami <habeeb.salami@gmail.com>
2020-02-13 08:46:45 +0100

    initial commit of the application

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>SMS2Profit</title>
+  <base href="http://127.0.0.1/sms2profit-lite/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.png">
+  <link rel="icon" type="image/x-icon" href="./assets/img/favicon.png">
+<link rel="stylesheet" href="styles.1b92bf64b77fda153f5d.css"></head>
+
+<body id="page-top">
+  <app-root></app-root>
+  <script src="/assets/vendor/jquery/jquery.min.js"></script>
+  <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="/assets/vendor/datatables/jquery.dataTables.min.js"></script>
+  <script src="/assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+  <script src="/assets/js/demo/datatables-demo.js"></script>
+  <script src="/assets/js/toastr.min.js"></script>
+  <script src="/assets/js/sms2profit.min.js"></script>
+<script src="runtime-es2015.edb2fcf2778e7bf1d426.js" type="module"></script><script src="runtime-es5.edb2fcf2778e7bf1d426.js" nomodule defer></script><script src="polyfills-es5.f067f5f913bead2cd167.js" nomodule defer></script><script src="polyfills-es2015.25ff13674b3056b14f77.js" type="module"></script><script src="main-es2015.1b7dd699b61495ce6d68.js" type="module"></script><script src="main-es5.1b7dd699b61495ce6d68.js" nomodule defer></script></body>
+
+</html>
